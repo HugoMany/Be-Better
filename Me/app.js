c5041cd92faba7215aea86738163ce6e64f5a869
HugoMany <90179566+HugoMany@users.noreply.github.com>
2023-03-22 16:00:09 +0100

    Add Nodes JS

--- Me/app.js
+++ Me/app.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://BBT:<password>@cluster0.54ar39o.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://BBT:0cka7EfxFSpfDkBk@cluster0.54ar39o.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
