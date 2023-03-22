39ce58330f95420e6252b14cde53f6f3ad6df826
EliottPeres <eliott.peres@student.junia.com>
2023-03-22 17:23:31 +0100

    petit formulaire avec recup des info par un JSON

--- Me/formulaire d'inscription/script.js
+++ Me/formulaire d'inscription/script.js
@@ -8,28 +8,34 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 var btn = document.getElementById("register");
 
-        btn.onclick = function () {
-            const nom = document.getElementById("nom").innerHTML;
-            const prenom = document.getElementById("prenom").innerHTML;
-            const username = document.getElementById("username").innerHTML;
-            const email = document.getElementById("email").innerHTML;
-            const telephone = document.getElementById("telephone").innerHTML;
-        }
+btn.onclick = function () {
+    let nom = document.getElementById("nom").value;
+    let prenom = document.getElementById("prenom").value;
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let telephone = document.getElementById("telephone").value;
+    let password = document.getElementById("password").value;
+    data = [];
+    data.push({ "nom": nom, "prenom": prenom, "username": username, "email": email, "telephone": telephone, "password": password });
+    data = JSON.stringify(data);
+    console.log(data);
+    modal.style.display = "none";
+}
