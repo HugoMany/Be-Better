1c214591d99264d89d4360a1abe1bfc6005a1738
Cespriet <90179216+Cespriet@users.noreply.github.com>
2023-05-09 13:45:53 +0200

    up

--- Me/meteo.js
+++ Me/meteo.js
@@ -1,5 +1,8 @@
 
-function func(){
+async function func(){
+   var callBackGetSuccess=function(data){
+      console.log("donnees api",data)
+   }
     if (navigator.geolocation) {
         console.debug('geolocalisation en cours...'); 
         navigator.geolocation.getCurrentPosition(getPosition, getError); 
@@ -7,13 +10,27 @@ function func(){
      else 
         document.getElementById("error").innerHTML="La géolocalisation n'est pas disponible avec votre navigateur.";
      
-     function getPosition(position) 
-     { 
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        console.log(latitude)
-        console.log(longitude)
-    } 
+      async function  getPosition(position) 
+      { 
+         const latitude = position.coords.latitude;
+         const longitude = position.coords.longitude;
+         console.log(latitude)
+         console.log(longitude)
+      
+      
+      var url ="https://geo.api.gouv.fr/communes?lat="+latitude+"&lon="+longitude+".04883&fields=code,nom,codesPostaux,surface,population,centre,contour"
+      let reponse = await fetch(url)
+      reponse = (await reponse.text());
+      reponse = JSON.parse(reponse)[0]
+
+      // console.log(reponse);
+      const codePostale = reponse.codesPostaux[0];
+      const ville = reponse.nom;
+      console.log(reponse.codesPostaux[0]);
+      console.log(reponse.nom);
+      url2="https://meteofrance.com/previsions-meteo-france/"+ville+"/"+codePostale
+
+    }
      
      function getError(error) 
      {
@@ -26,3 +43,4 @@ function func(){
         } 
      }; 
 }
+
