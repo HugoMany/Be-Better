09ea413f2f3dc980f7ca3ed8bc87a7d52bff000c
Cespriet <90179216+Cespriet@users.noreply.github.com>
2023-05-22 13:20:20 +0200

    sommeil

--- Me/sleep/sommeil.js
+++ Me/sleep/sommeil.js
@@ -22,6 +22,68 @@
 
 // };
 async function receiveSleep() {
+    const url = 'http://localhost:3000/api/user/caract/' + isConnected() + '/';
+    const options = {
+  
+        method: 'GET',
+  
+        headers: {
+  
+            'Content-Type': 'application/json'
+  
+        }
+  
+    };
+    try {
+  
+        const response = await fetch(url, options);
+  
+        const result = await response.text();
+        const jsonString = result;
+        const jsonRet = JSON.parse(jsonString);
+        createMyCharts(jsonRet);
+        console.log(jsonRet);
+        
+  
+    } catch (error) {
+  
+        console.error(error);
+  
+    }
+  
+  }
+function createMyCharts(json){ 
+const ctx = document.getElementById('myChart');
+
+  
+new Chart(ctx, {
+  
+    type: 'bar',
+    data: {
+        labels: json ["sleeps"].map(row=>new Date(parseInt(row.date)).toLocaleDateString('en-US', {  month: 'short', day: 'numeric', year: 'numeric' })),
+        datasets: [{
+            label: 'evolution du poid',
+            data: json ["sleeps"].map(row=>row.value),
+        borderWidth: 1
+        }]
+  },
+  options: {
+    elements: { 
+      
+      
+    borderColor: 'red',
+      
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      }
+    },
+    plugins : { legend :{display:false },},
+  }
+});
+}
+async function receiveSleep() {
     const url = '/api/user/oneuser/' + isConnected() + '/';
     const options = {
 
